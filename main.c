19f8ce6fb91b3c73c9002240dcb06aada53a229f
neofytr <imagination12344321.com@gmail.com>
2025-01-15 16:07:39 +0530

    ppm.md

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 #include <stdlib.h>
+#include <stdint.h>
+
+int save_ppm_file(uint32_t *pixels, size_t width, size_t height)
+{
+    if (!pixels || width == 0 || height == 0)
+    {
+        return -1;
+    }
+
+    
+}
 
 int main(void)
 {
